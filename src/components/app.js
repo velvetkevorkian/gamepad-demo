1236669d60278281bcd8b2642ad1f0eee9d24880
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2020-10-13 22:16:06 +0100

    Remove router and add gamepad component

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,10 @@
-import { h } from 'preact';
-import { Router } from 'preact-router';
-
-import Header from './header';
-
-// Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import { h } from 'preact'
+import Gamepad from './gamepad'
 
 const App = () => (
-	<div id="app">
-		<Header />
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-		</Router>
-	</div>
+  <div id="app">
+    <Gamepad />
+  </div>
 )
 
-export default App;
+export default App
